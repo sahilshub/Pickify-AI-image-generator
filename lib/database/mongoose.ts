aad4a3399858eb1da069043faa8f9aa78d575fc8
sahilshub <mohamedsahil753@gmail.com>
2024-02-17 19:33:51 +0530

    3 commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -10,17 +10,23 @@ interface MongooseConnection {
 let cached: MongooseConnection = (global as any).mongoose
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null }
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  }
 }
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn
 
-  if (!MONGODB_URL) throw new Error("Misssing MONGODB_URL")
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL")
 
   cached.promise =
     cached.promise ||
-    mongoose.connect(MONGODB_URL, { dbName: "pickify", bufferCommands: false })
+    mongoose.connect(MONGODB_URL, {
+      dbName: "pickifyai",
+      bufferCommands: false,
+    })
 
   cached.conn = await cached.promise
 
